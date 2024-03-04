5f2716923f46ae6ef94a4d89c46c63a85f872b90
BaarnRegeffio <162223435+BaarnRegeffio@users.noreply.github.com>
2024-03-04 19:43:11 -0300

    Add files via upload

---
+++ Opdrachten/Oefening 3/Oefen3.php
@@ -0,0 +1,17 @@
+<?php
+$getal1= 20;
+$getal2= 2;
+$som= $getal1 % $getal2;
+
+if($som == 0){
+    echo(" Goed gedaan! Getal is even");    
+} else {
+    echo("Getal is oneven");
+}
+
+
+
+
+
+
+?>
